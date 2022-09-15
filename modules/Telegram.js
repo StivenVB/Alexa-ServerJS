cdda29fdfb8188e8c153b82ca65b7891198c51ba
StivenVB <stiven.1701722520@ucaldas.edu.co>
2022-09-14 23:03:23 -0500

    status

--- modules/Telegram.js
+++ modules/Telegram.js
@@ -15,7 +15,7 @@ async function getRecurringOrders(identification, callback) {
         };
 
         let orderResponse = await REQUEST_PROMISE(orders);
-        console.log(orderResponse.status);
+        console.log(orderResponse.statusCode);
         callback(null, orderResponse);
 
         //return { status: 200, estado: true, mensaje: 'Exitoso', datos: orderResponse };
