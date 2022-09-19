56675564188340219b5d14fbd8ee0edc06f180d3
StivenVB <stiven.1701722520@ucaldas.edu.co>
2022-09-19 12:13:22 -0500

    test

--- service_layer/ServiceLayerClient.js
+++ service_layer/ServiceLayerClient.js
@@ -55,7 +55,7 @@ async function serviceLayerGet(prefix) {
         response = { status: 500, estado: false, mensaje: ex.message };
     }
 
-    console.log("response get: " + JSON.parse(response.value))
+    console.log("response get: " + JSON.stringify(response.value))
     return response;
 }
 
